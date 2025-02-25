620b7c273d5c0565ec372e4f8b311eb6eb19df03
Cory D <109181988+Nullalph@users.noreply.github.com>
2025-02-25 01:01:52 -0600

    basic display set up

---
+++ calculator.js
@@ -0,0 +1,38 @@
+// Arithmetic Functions
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+function percentage(a) {
+    return a/100;
+}
+
+
+
+function operate(op, x, y) {
+    switch(op) {
+        case '+':
+            return add(x, y);
+        case '-':
+            return subtract(x, y);
+        case '*':
+            return multiply(x, y);
+        case '/':
+            return divide(x, y);
+        case '%':
+            return percentage(x);
+    }
+}
